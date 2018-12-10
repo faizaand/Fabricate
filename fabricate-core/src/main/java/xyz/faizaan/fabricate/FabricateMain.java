d90083595161ac28880d1d65d30aa997a016cc6f
Your Name <fdatoo7@gmail.com>
2018-12-10 12:58:58 -0500

    Set up modules and build framework

---
+++ fabricate-core/src/main/java/xyz/faizaan/fabricate/FabricateMain.java
@@ -0,0 +1,5 @@
+package xyz.faizaan.fabricate;
+
+public class FabricateMain {
+
+}
---
+++ fabricate-spigot/src/main/java/xyz/faizaan/fabricate/spigot/FabricateSpigot.java
@@ -0,0 +1,11 @@
+package xyz.faizaan.fabricate.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FabricateSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
